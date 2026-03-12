7832e9bf03f85f2ac9f12e6678b1cb7dbe4fd773
Pete <pete@anvil.works>
2026-03-12 11:48:40 +0000

    Add unittest.mock

---
+++ test/unit3/test_mock/__init__.py
@@ -0,0 +1,11 @@
+from .testhelpers import *
+from .testcallable import *
+from .testmagicmethods import *
+from .testmock import *
+from .testpatch import *
+from .testsealable import *
+from .testsentinel import *
+from .testwith import *
+
+if __name__ == "__main__":
+    unittest.main()
